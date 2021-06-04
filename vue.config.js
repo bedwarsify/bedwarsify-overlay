67f44551334a48781deb010598ba61f5aff96e9b
Jakub Meysner <hello@jakubmeysner.me>
2021-06-04 23:04:51 +0200

    ci: set up Appx build

--- vue.config.js
+++ vue.config.js
@@ -6,20 +6,28 @@ process.env.VUE_APP_VERSION = require('./package.json').version
  */
 module.exports = {
   pluginOptions: {
-    /**
-     * @type {}
-     */
     electronBuilder: {
       preload: './src/preload.ts',
       outputDir: './dist',
       builderOptions: {
         appId: 'com.bedwarsify.overlay',
         productName: 'Bedwarsify Overlay',
+        win: {
+          target: ['nsis', 'appx'],
+        },
         mac: {
+          target: ['default'],
           category: 'public.app-category.games',
         },
         linux: {
+          target: ['AppImage', 'snap'],
           category: 'Game',
+        },
+        appx: {
+          applicationId: process.env.APPX_APPLICATION_ID,
+          identityName: process.env.APPX_IDENTITY_NAME,
+          publisher: process.env.APPX_PUBLISHER,
+          publisherDisplayName: process.env.APPX_PUBLISHER_DISPLAY_NAME,
         },
       },
     },
